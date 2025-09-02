af55471c21ede762a396bb91303636759ca61e87
Your Name <you@example.com>
2025-09-03 06:58:45 +0700

    работа с адапитивностью

--- footer.php
+++ footer.php
@@ -2,50 +2,49 @@
 
 <footer class="footer">
     <div class="container">
+        <!-- Первая строка -->
         <div class="footer__first">
-            <div class="footer__copyright">©2023 Yourcompany</div>
-            <a class="btn btn-primary" href="#">Purchase now</a>
+            <div class="footer__row">
+                <div class="footer__col footer__col--copyright">
+                    <div class="footer__copyright">©2023 Yourcompany</div>
+                </div>
+                <div class="footer__col footer__col--button">
+                    <a class="btn btn-primary footer__btn" href="#">Purchase now</a>
+                </div>
+            </div>
         </div>
+        
+        <!-- Вторая строка -->
         <div class="footer__second">
-            <?php
-            wp_nav_menu([
-                'theme_location' => 'top',
-                'depth' => 2,
-                'container' => false,
-                'menu_class' => 'footer__menu',
-                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                'walker' => new WP_Bootstrap_Navwalker(),
-                'link_class' => 'nav-link px-3 py-2 rounded'
-            ]);
-            ?>
-            <a class="footer__logo" href="/">Landing</a>
-            <ul class="footer__social">
-                <li>
-                    <a href="#">
-                        <i class="icon-facebook"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="icon-linkedin"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="icon-twitter"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="icon-youtube-play"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="icon-instagram"></i>
-                    </a>
-                </li>
-            </ul>
+            <div class="footer__row">
+                <div class="footer__col footer__col--menu">
+                    <div class="footer__menu-wrapper">
+                        <?php
+                        wp_nav_menu([
+                            'theme_location' => 'top',
+                            'depth' => 2,
+                            'container' => false,
+                            'menu_class' => 'footer__menu',
+                            'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker' => new WP_Bootstrap_Navwalker(),
+                            'link_class' => 'nav-link'
+                        ]);
+                        ?>
+                    </div>
+                </div>
+                <div class="footer__col footer__col--logo">
+                    <a class="footer__logo" href="/">Landing</a>
+                </div>
+                <div class="footer__col footer__col--social">
+                    <ul class="footer__social">
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#"><i class="icon-youtube-play"></i></a></li>
+                        <li><a href="#"><i class="icon-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
--- header.php
+++ header.php
@@ -9,50 +9,64 @@
     <?php wp_head(); ?>
 
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-            m[i].l = 1 * new Date();
-            for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103865815', 'ym');
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103951656', 'ym');
 
-        ym(103865815, 'init', { ssr: true, webvisor: true, clickmap: true, ecommerce: "dataLayer", accurateTrackBounce: true, trackLinks: true });
-    </script>
-    <noscript>
-        <div><img src="https://mc.yandex.ru/watch/103865815" style="position:absolute; left:-9999px;" alt="" /></div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
+    ym(103951656, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/103951656" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
      
     </head>
 
 <body>
-    <header class="header">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <?php
-                    wp_nav_menu([
-                        'theme_location' => 'top',
-                        'depth' => 2,
-                        'container' => false,
-                        'menu_class' => 'navbar-nav gap-lg-5 gap-3',
-                        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker' => new WP_Bootstrap_Navwalker(),
-                        'link_class' => 'nav-link px-3 py-2 rounded'
-                    ]);
-                    ?>
+   <header class="header">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <div class="header__grid">
+                <!-- Логотип -->
+                <div class="header__col header__col--logo">
+                    <a class="header__logo" href="/">Landing</a>
                 </div>
-                <a class="header__logo" href="/">Landing</a>
-                <a class="header__request btn btn-primary" href="#">Purchase now</a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                
+                <!-- Меню -->
+                <div class="header__col header__col--menu">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <?php
+                        wp_nav_menu([
+                            'theme_location' => 'top',
+                            'depth' => 2,
+                            'container' => false,
+                            'menu_class' => 'navbar-nav',
+                            'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker' => new WP_Bootstrap_Navwalker(),
+                            'link_class' => 'nav-link'
+                        ]);
+                        ?>
+                    </div>
+                </div>
+                
+                <!-- Кнопка Purchase -->
+                <div class="header__col header__col--button">
+                    <a class="header__request btn btn-primary" href="#">Purchase now</a>
+                </div>
+                
+                <!-- Кнопка меню -->
+                <div class="header__col header__col--toggler">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
+</header>
 
     <main class="main-content">
